a2fab60fc111efeaff57a05fe3f1e68ec34a52f2
irysius <lestersy@hotmail.com>
2014-05-04 11:17:46 -0400

    added GET: /usage/:imageid

--- routes/usage.js
+++ routes/usage.js
@@ -8,7 +8,56 @@ usage.post('/create', function (req, res, next) {
 	var permissions = req.body.permissions;
 
 	database.createUsage(userid, imageid, permissions, function (result) {
+		console.log('createUsage');
+		if (!result.result) res.json(result);
+
+		console.log(imageid);
+		database.getImage(imageid, function (result2) {
+			console.log('createUsage2');
+			if (result2.result) {
+				var userid = result2.data.userid;
+				console.log(imageid);
+				database.getUser(userid, function (result3) {
+					console.log('createUsage3');
+					console.log(result3.data.email);
+					if (result3.result) {
+						var email = result3.data.email;
+						console.log('emailing');
+						//email(email);
+					}
+				})
+			}
+		}, true);
+		
 		res.json(result);
 	});
 })
+
+usage.get('/:imageid(\\d+)', function (req, res, next) {
+	var imageid = req.params.imageid;
+
+	database.getUsages(imageid, function (result) {
+		res.json(result);
+	})
+})
+
+function email(email) {
+	var transport = nodemailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			user: 'pickmehhsickkids@gmail.com',
+			pass: 'pickme123'
+		}
+	})
+	var mailOptions = {
+		from: 'pickmehhsickkids@gmail.com',
+		to: email,
+		subject: 'Medical Image Usage',
+		text: 'Your physician xyz just used your image for the following purposes'
+	}
+	transport.sendMail(sendOptions, function (err, status) {
+
+	});
+}
+
 module.exports = usage;
